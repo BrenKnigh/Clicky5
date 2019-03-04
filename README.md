faeed526a49a186994b2f1a120c902fefd859788
WaffleCode <brenknigh117@gmail.com>
2019-03-04 03:47:55 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Hollow Knight Guessing Game!
-Built to use React, be interactive, and up to date.
---
+++ precache-manifest.a4af71114440c07aa587f42d191a06b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "54203d5e170b2491ec4f",
+    "url": "/static/js/main.54203d5e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "54203d5e170b2491ec4f",
+    "url": "/static/css/main.75e44c16.chunk.css"
+  },
+  {
+    "revision": "11b08c6dd9c069dc91d89128a15774a6",
+    "url": "/index.html"
+  }
+];
